6020f91540434cd7ccc33f860cf0cae7e9f4dfe7
Melody Bugnon <melodybugnon@gmail.com>
2024-03-06 10:11:52 +0100

    sign up

--- sba/urls.py
+++ sba/urls.py
@@ -21,6 +21,6 @@ from main import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('main.urls')),
-    path('funct/', include('functionalities.urls')),
+    path('accounts/', include('functionalities.urls')),
     path("accounts/", include("django.contrib.auth.urls"))
 ]
