aeb3b92f2685da6ea9104b304b1e956fff6cf2eb
Melody Bugnon <melodybugnon@gmail.com>
2024-03-08 15:14:24 +0100

    html signup à faire

--- functionalities/models.py
+++ functionalities/models.py
@@ -1,9 +1,16 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
-    pass
-
 class Functionalities(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Ajoutez d'autres champs si nécessaire pour votre application
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False
+    )
+    description = models.TextField(
+        max_length=500,
+        null=False,
+        blank=False
+    )
+
+    def __str__(self):
+        return f'{self.name} : {self.description}'
